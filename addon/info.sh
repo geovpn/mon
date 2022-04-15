45907e118abcdf778fbebd8ef1c3aea9a92a2bc9
Muhammad Amin <96859296+geovpn@users.noreply.github.com>
2022-04-15 10:51:42 +0800

    Add files via upload

---
+++ addon/info.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+clear
+neofetch --ascii_distro virus echo && echo "AutoScript Mod By Geo Project" &&  echo "t.me/sampiiiiu" 
+
+echo "AutoScript VPN Mod By Geo Project"
+echo "t.me/sampiiiiu"
